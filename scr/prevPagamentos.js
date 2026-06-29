3d387ffdb89b88b8f80fcc39f44274ae6f30a984
HallanBrandt <hjulianoamaral@gmail.com>
2026-06-29 15:48:49 -0300

    funções de mostrar e reirar previa adicionados, estlização previa completada, imagens previa adicionados

--- scr/prevPagamentos.js
+++ scr/prevPagamentos.js
@@ -1,17 +1,22 @@
 let x = document.getElementById("btn-fecha-prev");
 let btn = document.getElementById("prevPagamentos");
-
 let divPrevia = document.getElementById("previa");
 
-
-
 export function previa() {
+    // botão de fechar
     x.addEventListener("click", () => {
         divPrevia.style.display = "none";
-    })
+    });
 
+    // botão que abre
     btn.addEventListener("click", () => {
         divPrevia.style.display = "block";
-    })
+    });
 
+    // clique fora da div
+    window.addEventListener("click", (e) => {
+        if (!divPrevia.contains(e.target) && !btn.contains(e.target)) {
+            divPrevia.style.display = "none";
+        }
+    });
 }
