a7d3fe38b701706569e6945405426a6f80164298
HallanBrandt <hjulianoamaral@gmail.com>
2026-06-29 20:45:58 -0300

    inseridas sessões de sabores e bebidas, função que cria objetos pizza e objeto carrainho criados

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+import { rendLista } from "./pizza.js";
 import { horario } from "./scr/horas.js";
 import { previa } from "./scr/prevPagamentos.js";
+
+export let editar = {pizza: 0};
+export let inicio = document.getElementById("inicio");
+export let bebidas = document.getElementById("bebidas");
+export let sabores = document.getElementById("sabores");
 
 horario();
 
 previa();
+
+rendLista();
